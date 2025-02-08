2b5003d71ad8bff5130796db008d958a9df7bca2
miniblack_Tw <134146166+miniblackTW@users.noreply.github.com>
2025-02-08 22:58:12 +0800

    Update BlockClearAnimation.java
    Added boolean "isClearing"

--- src/main/java/com/njdge/fastbuilder/profile/BlockClearAnimation.java
+++ src/main/java/com/njdge/fastbuilder/profile/BlockClearAnimation.java
@@ -6,19 +6,51 @@ import org.bukkit.block.Block;
 import static com.njdge.fastbuilder.utils.Tasks.runLater;
 
 public class BlockClearAnimation {
+    private static boolean isClearing = false;
+
+    public static boolean isClearing() {
+        return isClearing;
+    }
+
     public static void sequential(PlayerProfile profile) {
         int totalBlocks = profile.getPlacedBlocks().size();
+        if (totalBlocks == 0) return;
+
+        isClearing = true;
+
         int[] counter = {0};
         profile.getPlacedBlocks().forEach(location -> {
             Block block = location.getBlock();
-            runLater(() -> block.setType(Material.AIR), (long) (totalBlocks * ((double) counter[0]++ / totalBlocks)));
+            long delay = (long) (totalBlocks * ((double) counter[0] / totalBlocks));
+            
+            runLater(() -> {
+                block.setType(Material.AIR);
+                if (++counter[0] == totalBlocks) {
+                    isClearing = false;
+                }
+            }, delay);
+
+            counter[0]++;
         });
     }
 
     public static void clear(PlayerProfile profile) {
+        int totalBlocks = profile.getPlacedBlocks().size();
+        if (totalBlocks == 0) return;
+
+        isClearing = true;
+
+        int[] counter = {0};
         profile.getPlacedBlocks().forEach(location -> {
             Block block = location.getBlock();
-            runLater(() -> block.setType(Material.AIR), 0);
+            runLater(() -> {
+                block.setType(Material.AIR);
+                if (++counter[0] == totalBlocks) {
+                    isClearing = false;
+                }
+            }, 0);
+
+            counter[0]++;
         });
     }
 }
